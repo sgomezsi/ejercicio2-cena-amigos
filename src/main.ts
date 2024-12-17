32796169c2d274dbee6e5fc17a80af3da34470c2
sandra Gomez <sgomezsi@gmail.com>
2024-12-17 09:44:39 +0100

    segundo ejercicio

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
-import "./style.css";
+console.log(`Cada comensal deberá pagar ${(120 - 18) / 6} € por la comida.`);
 
-console.log("Hello Typescript!");
+// (120 - 18): Restamos el coste de las bebidas del total de la cena (120 € - 18 € = 102 €).
+//    / 6: Dividimos el resultado entre los 6 amigos.
